a0e0173af564551fc3d31a5b551ae98ebfbf17bb
kirkjoserey <kirkjoserey@gmail.com>
2026-05-18 14:27:30 -0300

    feat: estructura maven multimodulo con spring boot 3.5.14 y java 25

---
+++ baqueano-backend/src/main/java/ar/com/baqueano/BaqueanoApplication.java
@@ -0,0 +1,12 @@
+package ar.com.baqueano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaqueanoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaqueanoApplication.class, args);
+    }
+}
